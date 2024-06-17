2240c57e458038dc73702991da751a5bced92f74
saroj13278 <saroj.pandarkl@gmail.com>
2024-06-17 09:27:23 +0530

    get links

---
+++ getlinks/getlinks.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def make_authenticated_request(api_url, bearer_token):
+    headers = {
+        'Authorization': f'Bearer {bearer_token}',
+        'Content-Type': 'application/json',  # Adjust content type if needed
+    }
+   
+
+    try:
+        response = requests.get(api_url, headers=headers)
+
+        # Check if the request was successful (status code 201)
+        if response.status_code == 200:
+            print("Request successful. Response:")
+            print(response.json())
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+# Replace 'YOUR_API_URL' with the actual API endpoint you want to access
+api_url = 'https://api.zephyrscale.smartbear.com/v2/testcases/DEV-T117/links'
+
+
+file_path = 'token.txt'
+
+# Open the file in read mode
+with open(file_path, 'r') as file:
+    # Read the API token from the file
+    bearer_token = file.read().strip()
+
+make_authenticated_request(api_url, bearer_token)
